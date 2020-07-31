13260a30c4ce2bfc6fac8d9d4c71fe993abacf77
Sameer <sameerpatil@boomi.com>
2020-07-31 13:52:38 -0400

    Added More tests

--- src/test/java/com/demoblaze/HomeTest.java
+++ src/test/java/com/demoblaze/HomeTest.java
@@ -8,9 +8,46 @@ import org.testng.annotations.Test;
 public class HomeTest extends TestSetup {
 
     @Test
-    public void testHome(){
-        System.out.println("Inside Test");
+    public void testHome_1(){
         System.out.println(driver.getCurrentUrl());
         Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testHome_2(){
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testHome_3(){
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testHome_4(){
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertTrue(true);
+    }
+
+
+    @Test
+    public void testHome_5(){
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertTrue(false);
+    }
+
+
+    @Test
+    public void testHome_6(){
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testHome_7(){
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertTrue(false);
     }
 }
